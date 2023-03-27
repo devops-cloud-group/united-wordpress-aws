79be52c59f0ae6143cacc6d550f144598a552bd7
hasanuckun <devophasanuckun@gmail.com>
2023-03-27 20:47:18 +0000

    added route53 with outputs

---
+++ route53.tf
@@ -0,0 +1,21 @@
+resource "aws_route53_record" "www" {
+  zone_id = "Z02872513CHOV2LLJCBMY"
+  name    = "blog.hasanuckun.com"
+  type    = "A"
+  ttl     = 300
+  records = ["127.0.0.1"]
+
+}
+
+# output for domain 
+output "zone_id" {
+  value = aws_route53_record.www.zone_id
+}
+
+output "name" {
+  value = aws_route53_record.www.name
+}
+
+output "records" {
+  value = aws_route53_record.www.records
+}
