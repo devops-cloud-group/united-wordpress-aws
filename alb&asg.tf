94309c10b899df420c1fabeebf8c261d7e50e2e0
EC2 Default User <ec2-user@ip-172-31-18-180.eu-west-1.compute.internal>
2023-03-29 03:20:25 +0000

    new code version 1

--- alb&asg.tf
+++ alb&asg.tf
@@ -1,90 +1,125 @@
-#  Launch configuration
-resource "aws_launch_configuration" "lunchconfig" {
-  image_id        = data.aws_ami.latest.id
-  instance_type   = var.instance_type
-  security_groups = [aws_security_group.web.id]
-  key_name = var.key_name1
+#------------------------------------------------------------------
+# Provision Highly Availabe Web Cluster
+# Create:
+#    - Security Group for Web Server and ALB
+#    - Launch Template with Auto AMI Lookup
+#    - Auto Scaling Group using 3 Availability Zones
+#    - Application Load Balancer in 3 Availability Zones
+#    - Application Load Balancer TargetGroup
+#------------------------------------------------------------------
+provider "aws" {
+  region = var.region # Region specified in varible.tf file
 
+  default_tags {
+    tags = {
+      CreatedBy = "Terraform"
+    }
+  }
+}
+
+
+data "aws_availability_zones" "working" {}
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["137112412989"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+
+#-------------------------------------------------------------------------------
+resource "aws_security_group" "web" {
+  name   = "Web Security Group"
+  vpc_id = aws_vpc.main.id
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Web Security Group"
+  }
+}
+
+#-------------------------------------------------------------------------------
+resource "aws_launch_template" "web" {
+  name                   = "WebServer-Highly-Available-LT"
+  image_id               = data.aws_ami.latest_amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  user_data              = filebase64("${path.module}/user_data.sh")
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                = "WebServer-Highly-Available-ASG-Ver-${aws_launch_template.web.latest_version}"
+  min_size            = 2
+  max_size            = 2
+  min_elb_capacity    = 2
+  health_check_type   = "ELB"
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  target_group_arns   = [aws_lb_target_group.web.arn]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = aws_launch_template.web.latest_version
+  }
+
+  dynamic "tag" {
+    for_each = {
+      Name   = "WebServer in ASG-v${aws_launch_template.web.latest_version}"
+      TAGKEY = "TAGVALUE"
+    }
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
   lifecycle {
     create_before_destroy = true
   }
 }
 
-#  ASG
-resource "aws_autoscaling_group" "asg" {
-  name                 = "example-asg"
-  launch_configuration = aws_launch_configuration.lunchconfig.id
-  vpc_zone_identifier  = [var.private_subnet_ids]
-  min_size             = 1
-  max_size             = 99
-  desired_capacity     = 1
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-#  ALB
-resource "aws_lb" "alb" {
-  name               = "alb-lb"
+#-------------------------------------------------------------------------------
+resource "aws_lb" "web" {
+  name               = "WebServer-HighlyAvailable-ALB"
   load_balancer_type = "application"
-  subnets            = var.public_subnet_ids
-  security_groups    = [aws_security_group.example_sg.id]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  security_groups    = [aws_security_group.web.id]
+  subnets            = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
 }
 
-#  ALB Target Group
-resource "aws_lb_target_group" "alb-tgroup" {
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = var.vpc_id
-
-  lifecycle {
-    create_before_destroy = true
-  }
+resource "aws_lb_target_group" "web" {
+  name                 = "WebServer-HighlyAvailable-TG"
+  vpc_id               = aws_default_vpc.default.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 10 # seconds
 }
 
-# Attach Target Group to ASG
-resource "aws_autoscaling_attachment" "example" {
-  autoscaling_group_name = aws_autoscaling_group.asg.name
-  alb_target_group_arn   = aws_lb_target_group.alb-tgroup.arn
-}
-
-# Create ALB Listener
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.alb-tgroup.arn
     type             = "forward"
-  }
-
-  lifecycle {
-    create_before_destroy = true
+    target_group_arn = aws_lb_target_group.web.arn
   }
 }
 
-resource "aws_security_group" "example_sg" {
-  name_prefix = "example_sg_"
-  vpc_id      = var.vpc_id
-
-  ingress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#-------------------------------------------------------------------------------
+output "web_loadbalancer_url" {
+  value = aws_lb.web.dns_name
 }
