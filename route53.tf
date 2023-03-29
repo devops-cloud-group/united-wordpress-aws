90a84c93f4e9ac497845eec361a72b5b750232a7
hasanuckun <devophasanuckun@gmail.com>
2023-03-29 19:55:52 +0000

    made changes on route53

--- route53.tf
+++ route53.tf
@@ -1,12 +1,22 @@
+data "aws_route53_zone" "hosted_zone" {
+  name = "hasanuckun.com"
+}
+
+# terraform aws route 53 record
 resource "aws_route53_record" "www" {
   zone_id = "Z02872513CHOV2LLJCBMY"
-  name    = "blog.hasanuckun.com"
+  name    = "hasanuckun.com"
   type    = "A"
-  ttl     = 300
-  records = ["127.0.0.1"]
-  #records = [aws_lb.MYALB.dns_name] 
 
 }
+
+
+#alias {
+#  name                   =  #ALB details will be added here 
+#  zone_id                =  #ALB zone_id will be added here 
+# evaluate_target_health = true 
+# }
+
 
 # output for domain 
 output "zone_id" {
