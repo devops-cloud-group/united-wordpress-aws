ae80180b2e93fc09cf8a00171fba5ae985e43592
hasanuckun <devophasanuckun@gmail.com>
2023-03-30 06:05:26 +0000

    changed route53

--- README.md
+++
@@ -1,3 +0,0 @@
-- We already have domain and ID registered in AWS account
-
-- referenced by https://github.com/azeezsalu/terraform-project-reference/blob/bbbfb17355096cf53dee0add91c2f1801e200aa9/route-53%20reference.tf
--- route53.tf
+++ route53.tf
@@ -1,32 +1,23 @@
-data "aws_route53_zone" "hosted_zone" {
-  name = "hasanuckun.com"
-}
-
-# terraform aws route 53 record
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "db" {
   zone_id = "Z02872513CHOV2LLJCBMY"
-  name    = "hasanuckun.com"
-  type    = "A"
+  name    = "wordpressdb.hasanuckun.com"
+  type    = "CNAME"
+  ttl     = "300"
+  # records = [
+  #   rds.name.address
+  # ]
 
 }
-
-
-#alias {
-#  name                   =  #ALB details will be added here 
-#  zone_id                =  #ALB zone_id will be added here 
-# evaluate_target_health = true 
-# }
-
 
 # output for domain 
 output "zone_id" {
-  value = aws_route53_record.www.zone_id
+  value = aws_route53_record.db.zone_id
 }
 
 output "name" {
-  value = aws_route53_record.www.name
+  value = aws_route53_record.db.name
 }
 
 output "records" {
-  value = aws_route53_record.www.records
+  value = aws_route53_record.db.records
 }
