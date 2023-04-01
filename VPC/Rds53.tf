81acc12f8dbc99eaaacb653d16199e7d59c77c9f
stanislav4907 <120432275+stanislav4907@users.noreply.github.com>
2023-04-01 02:15:51 -0400

    Fixed RDS readers to 1-2-3 (#12)
    Co-authored-by: stanislav krotiuk <“biotopler@gmail.com”>

--- VPC/Rds53.tf
+++ VPC/Rds53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "db_writer" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "writer.selected"
+  name    = "writer"
   type    = "CNAME"
   ttl     = 300
   records = [aws_rds_cluster_instance.writer[0].endpoint]
@@ -11,7 +11,7 @@ resource "aws_route53_record" "db_writer" {
 resource "aws_route53_record" "reader" {
   count   = local.readers
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "reader${count.index+1}.selected"
+  name    = "reader${count.index+1}"
   type    = "CNAME"
   ttl     = 300
   records = [aws_rds_cluster_instance.reader[count.index].endpoint]
