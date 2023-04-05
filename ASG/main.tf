cfb4689f6b39a5587089336dd0d0161562b0f9ac
oliaasem <120681042+oliaasem@users.noreply.github.com>
2023-04-04 23:51:49 -0500

    ec2.tf file  added to main.tf

--- ASG/main.tf
+++ ASG/main.tf
@@ -17,6 +17,27 @@ provider "aws" {
   # }
 }
 
+// find latesst ami
+data "aws_ami" "latest" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["${var.os_version}-*-hvm-*-gp2"]
+  }
+
+  owners = ["amazon"]
+}
+
+resource "aws_key_pair" "web" {
+  key_name   = var.key_name1
+  public_key = file(var.public_key)
+}
+
+resource "aws_key_pair" "server" {
+  key_name   = var.key_name2
+  public_key = file(var.public_key)
+
 # data "aws_availability_zones" "available" {}
 
 
