5920798c5769be223eebac8acfb4ae2472bd5710
Sergei Litovchenko <serglit72@gmail.com>
2023-04-05 20:46:05 -0700

    edited gitignore in VPC folder

--- VPC/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" { 
-        bucket = "terraform-tfstate-wordpress"
-        key    = "backend/terraform.tfstate"
-        region = "us-west-1"
-        dynamodb_table = "terraform-prod-lock"
-    } 
-}
