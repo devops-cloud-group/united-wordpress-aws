8cc9d57fc19d9c66cb0b7a4039399b1240d2d1aa
Sergei Litovchenko <serglit72@gmail.com>
2023-09-29 00:34:41 -0700

    Version 0.3.0 - with autoinstall S3 & DynamoDB

--- ASG/backend-prod.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-tfstate-prod-877868457937"
-    key            = "backend/prod/terraform.tfstate"
-    region         = "us-west-1"
-    dynamodb_table = "terraform-backend-prod-877868457937"
-  }
-}
